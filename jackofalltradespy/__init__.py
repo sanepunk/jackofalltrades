2c1373f12cbddb73ddd1aa9dac69b3a82ab8dbb2
Ojas Patil <punk00pp@gmail.com>
2024-05-23 19:38:01 +0530

    Removed src and app folders for creating core tensorflow styled library

--- jackofalltradespy/__init__.py
+++ jackofalltradespy/__init__.py
@@ -1,2 +1,3 @@
-from .app.src.Models import LinearRegression, LogisticRegression
-from .app.test.tests import LinearTest, LogisticTest
+from app.src.Models import LinearRegression, LogisticRegression
+from app.test.tests import LinearTest, LogisticTest
+from app.src.Errors import Error
--- jackofalltradespy/app/__init__.py
+++
@@ -1,1 +0,0 @@
-from .test.tests import LinearTest, LogisticTest
